307309947bae2f80ba397773aae15b38940ad925
drkbot <85766119+botpage@users.noreply.github.com>
2021-07-29 22:42:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -25,18 +25,6 @@ menu_item.forEach((item) => {
 });
 
 
-// Scroll up
-document.getElementById("button-up").addEventListener("click", scrollUp);
-function scrollUp(){
-    var currentScroll = document.documentElement.scrollTop;
-
-    if (currentScroll > 0){
-        window.requestAnimationFrame(scrollUp);
-        window.scrollTo (0, currentScroll - (currentScroll / 5));
-    }
-}
-
-
 ///
 buttonUp = document.getElementById("button-up");
 window.onscroll = function(){
