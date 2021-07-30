28b8ef460604caef950c77fe0df93b119beb06a1
drkbot <85766119+botpage@users.noreply.github.com>
2021-07-30 00:54:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -35,3 +35,15 @@ var myButton = document.getElementById("btn-up");
                 myButton.style.display = "none";
             }
         }
+
+document.getElementById("btn-up").addEventListener("click", scrollUp);
+
+function scrollUp(){
+
+    var currentScroll = document.documentElement.scrollTop;
+
+    if (currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - (currentScroll / 5));
+    }
+}
